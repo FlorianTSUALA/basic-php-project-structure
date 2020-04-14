76c3700ef0a2607623eccef01771877672f30fda
Douglas Medeiros <medeirosinacio@outlook.com>
2020-04-14 03:17:04 -0300

    set timeout and composer cache

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 docker-compose up -d --force-recreate --build --remove-orphans
-docker exec phpfpm composer update
+
+# COMPOSER UPDATE
+docker run --rm --interactive --tty \
+  --volume $PWD/../:/app \
+  --volume ~/.composer/cache:/composer/cache \
+  --user $(id -u):$(id -g) \
+  composer update
